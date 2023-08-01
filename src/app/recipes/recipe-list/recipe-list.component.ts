ac13d671f1db38dd44ee0a2aa64d5aca98c11574
zeeshan <za920829@gmail.com>
2023-08-01 17:24:25 +0500

    main - merge branch "feature/project-setup"

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'Description for the test recipe',
+      'https://hips.hearstapps.com/hmg-prod/images/easy-dinner-recipes-1676057761.jpeg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'Description for the test recipe',
+      'https://hips.hearstapps.com/hmg-prod/images/easy-dinner-recipes-1676057761.jpeg'
+    ),
+  ];
 
+  ngOnInit(): void {}
 }
