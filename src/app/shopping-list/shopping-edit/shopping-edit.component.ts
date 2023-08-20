7394c547a28bf0c7e330fd73a10c78e98a86feb7
Zeeshan Ahmad <za920829@gmail.com>
2023-08-20 12:55:05 -0700

    main -  merge branch "feature/090-add-ingredients"

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+// model
+import { Ingredient } from '../ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
 
+  @Output() addedIngredient = new EventEmitter<Ingredient>()
+
+  onAddIngredient() {
+    const igName = this.nameInputRef.nativeElement.value;
+    const igAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(igName, igAmount);
+    if (newIngredient.amount && newIngredient.name) {
+      this.addedIngredient.emit(newIngredient);
+    } else {
+      alert('Please Enter Ingredient to be added!')
+    }
+    this.nameInputRef.nativeElement.value = '';
+    this.amountInputRef.nativeElement.value = '';
+  }
 }
