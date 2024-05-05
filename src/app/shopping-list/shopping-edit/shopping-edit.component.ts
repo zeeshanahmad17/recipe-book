837051bf075df390a79ab470af8052858cba74c5
Zeeshan Ahmad <za920829@gmail.com>
2024-05-06 01:03:54 +0500

    main - merge - 16 - forms added til 220

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,30 +1,31 @@
-import { Component, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  EventEmitter,
+  Output,
+} from '@angular/core';
 // model
 import { Ingredient } from '../ingredient.model';
 import { ShoppingListService } from '../shoppingList.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+  constructor(private slService: ShoppingListService) {}
 
-
-  constructor(private slService: ShoppingListService) { }
-
-  onAddIngredient() {
-    const igName = this.nameInputRef.nativeElement.value;
-    const igAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(igName, igAmount);
+  onAddIngredient(form: NgForm) {
+    const values = form.value;
+    const newIngredient = new Ingredient(values.name, values.amount);
+    debugger;
     if (newIngredient.amount && newIngredient.name) {
       this.slService.addIngrendient(newIngredient);
     } else {
-      alert('Please Enter Ingredient to be added!')
+      alert('Please Enter Ingredient to be added!');
     }
-    this.nameInputRef.nativeElement.value = '';
-    this.amountInputRef.nativeElement.value = '';
   }
 }
